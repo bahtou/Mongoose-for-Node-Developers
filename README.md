9bcd66a6eda16da70e4ac0e7313e3e8133f6171a
bahtou <bahtou@nym.hush.com>
2012-11-04 13:36:39 -0500

    hmwk2 starting

--- README.md
+++ README.md
@@ -1,30 +1,12 @@
-**10genEducation**
-==============
+**WEEK 2 Homework**
+===================
 
-A course **M101: MongoDB for Developers** is being offered at [10gen](https://education.10gen.com/).  
+**hm2.2**
+---------
 
-The course is using `python` to stage working with mongo database.  
-I am currently working with `node.js` so it's only logical that I mimic the course using `node.js`.  
-
-Feel free to follow along, contribute and `donkey.punch` if my code begins to anything like a flying spaghetti monster.
-
-
-**Requirements**
-----------------
-Through the course I will be using:
-
-*  [Express.js](http://expressjs.com/) v3.0.0
-*  [Mongoose.js](http://mongoosejs.com/index.html) v3.3.1
-*  and of course [Node.js](http://nodejs.org/) v0.8.14
+> Write a program in the language of your choice that will remove the lowest homework score for each student from the dataset that you imported in HW 2.1.
 
 
 
-**Install**
-----------
-
-*    Clone this repo `https://github.com/bahtou/Mongoose-for-Node-Developers.git`
-*    Switch between the weekly branches to follow along `git checkout week_01`, `git checkout week_02`, ... etc.  
-
-**Notices**
------------
-Every week will have its own `README` so make sure to check them out for additional notes about the homework for that given week.
+**hm2.3**
+---------
---
+++ hw2-2.js
@@ -0,0 +1,70 @@
+/*
+  homework 2.2
+  Write a program in the language of your choice that will remove the lowest homework 
+  score for each student from the dataset that you imported in HW 2.1.
+*/
+
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema;
+
+var db = mongoose.connect('localhost', 'students', '27017').connection;
+
+db.on('error', console.error.bind(console, 'Could not connect to mongo server'));
+
+// define schema -- can be `new Schema;`
+var gradesSchema = new Schema(
+                            { student_id: Number,
+                              type: String,
+                              score: Number}
+                          , { safe: true}
+                            );
+
+// model handler
+var grades = db.model('grades', gradesSchema);
+
+// instance of query
+var data = grades.find({type: 'homework'}).sort('student_id -score');
+
+data.exec(function(err, docs) {
+  // `docs` is an array populated with objects
+  if (err) {console.error('Error on db execution: ' + err); return;}
+
+  var len = docs.length - 1;
+  // search through and remove documents
+  for(i=0; i < len; i++) {
+    if (docs[i].student_id == docs[i + 1].student_id) {
+      grades.remove({_id: docs[i+1]._id}).exec();
+      }
+    }
+});
+
+/* 
+  The code below is unnecessary.  You can check if the above execution worked by
+  using the Mongo Shell.
+  
+  Because of async nature nature of JS the code will be executed while waiting on
+  `data.exec` to finish, hence the `setTimeout`
+*/
+
+// wait 5 seconds and then execute
+setTimeout( logIT, 5000);
+
+function logIT ()
+{
+  grades.find(function(err, docs) {
+    console.log(docs.length);
+    grades.find().sort('-score').skip(100).limit(1).exec(function(err, doc) {
+      console.log(doc);
+      grades.find().select({'student_id':1, 'type':1, 'score':1, '_id':0}).sort({student_id: 1, score: 1}).limit(5).exec(function(err, docs) {
+        console.log(docs);
+        db.close();
+      });
+    });
+  });
+}
+
+/*
+  Execute the code below in the MONGO SHELL to get your answer
+  db.grades.aggregate({'$group':{'_id':'$student_id', 'average':{$avg:'$score'}}}, {'$sort':{'average':-1}}, {'$limit':1})
+*/
+
