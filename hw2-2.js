da921403c69cd5856e8ed3f423e03f8e7a24f694
bahtou <bahtou@nym.hush.com>
2012-11-11 14:47:12 -0500

    added STREAMing

--- hw2-2.js
+++ hw2-2.js
@@ -7,11 +7,11 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
 
-var db = mongoose.connect('localhost', 'students', '27017').connection;
-
-db.on('error', console.error.bind(console, 'Could not connect to mongo server'));
-
-// define schema -- can be `new Schema;`
+// mongoose.set('debug', true);
+mongoose.connect('localhost', 'students', '27017');
+mongoose.connection.on('error', console.error.bind(console, 'Could not connect to mongo server'));
+mongoose.connection.on('open', function() {console.log('connected to mongodb'); });
+// define schema -- can be `new Schema(); -- safe: true --> is the default`
 var gradesSchema = new Schema(
                             { student_id: Number,
                               type: String,
@@ -20,44 +20,62 @@ var gradesSchema = new Schema(
                             );
 
 // model handler
-var grades = db.model('grades', gradesSchema);
+var grades = mongoose.model('grades', gradesSchema);
 
 // instance of query
-var data = grades.find({type: 'homework'}, {safe: true}).sort('student_id -score');
+// var data = grades.find({type: 'homework'}).sort('student_id -score');
 
-data.exec(function(err, docs) {
-  // `docs` is an array populated with objects
-  if (err) {console.error('Error on db execution: ' + err); return;}
+// data.exec(function(err, docs) {
+//   // `docs` is an array populated with objects
+//   if (err) {console.error('Error on db execution: ' + err); return;}
 
-  var len = docs.length - 1;
-  // search through and remove documents
-  for(i=0; i < len; i++) {
-    if (docs[i].student_id == docs[i + 1].student_id) {
-      grades.remove({_id: docs[i+1]._id}).exec();
+//   var len = docs.length - 1;
+//   // search through and remove documents
+//   for(i=0; i < len; i++) {
+//     if (docs[i].student_id == docs[i + 1].student_id) {
+//       grades.remove({_id: docs[i+1]._id}).exec();
+//       }
+//     }
+// });
+
+/*
+  STREAM-ON
+*/
+  var datum = [];
+  var stream = grades.find({type: 'homework'}).sort('student_id -score').stream();
+  
+  stream.on('data', function(doc) {
+    datum.push(doc);
+  });
+  
+  stream.on('close', function() {
+    var len = datum.length -1;
+    var i = 0;
+    // while-loop blocks
+    while (i < len) {
+      if (datum[i].student_id == datum[i + 1].student_id) {
+        console.log('yep');
+        grades.remove({_id: datum[i+1]._id}).exec();
       }
+      i++;
     }
-});
+    logIT();
+  });
 
 /*
   The code below is unnecessary.  You can check if the above execution worked by
   using the Mongo Shell.
-  
-  Because of async nature nature of JS the code will be executed while waiting on
-  `data.exec` to finish, hence the `setTimeout`
 */
-
-// wait 5 seconds and then execute
-setTimeout( logIT, 5000);
 
 function logIT ()
 {
   grades.find(function(err, docs) {
-    console.log(docs.length);
+    console.log('length of docs: ' + docs.length);
     grades.find().sort('-score').skip(100).limit(1).exec(function(err, doc) {
       console.log(doc);
       grades.find().select({'student_id':1, 'type':1, 'score':1, '_id':0}).sort({student_id: 1, score: 1}).limit(5).exec(function(err, docs) {
         console.log(docs);
-        db.close();
+        mongoose.disconnect();
       });
     });
   });
