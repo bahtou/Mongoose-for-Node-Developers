8f2a2dd15e701c8da4014b8f6b93ee6133dbbb18
bahtou <bahtou@nym.hush.com>
2012-11-16 13:21:56 -0500

    modified aggregate

--- connection_test.js
+++ connection_test.js
@@ -3,19 +3,28 @@ var mongoose = require('mongoose')
 
 mongoose.connect('localhost', 'students', '27017');
 mongoose.connection.on('error', console.error.bind(console, 'Could not connect to mongo server'));
+console.log('mongoose version: %s', mongoose.version);
 
 // define schema -- can be `new Schema;`
-var gradesSchema = new Schema(
-                            { student_id: Number,
-                              type: String,
-                              score: Number}
-                          , { safe: true}
-                            );
+// var gradesSchema = new Schema(
+//                             { student_id: Number,
+//                               // type: String,
+//                               score: Number}
+//                           , { safe: true}
+//                           , {_id: false}
+//                             );
 
 // model handler
+var gradesSchema = new Schema({}, {_id: false});
 var grades = mongoose.model('grades', gradesSchema);
-grades.find({}).exec(function(err, data) {
-  if (!err) return console.log('hi');
+
+grades.find({}).limit(10).exec(function(err, data) {
+  if (err) {console.log('hi' + err); return mongoose.disconnect();}
+  console.log(data);
 });
 
-grades.aggregate({'$group':{'_id':'$student_id', 'average':{$avg:'$score'}}}, {'$sort':{'average':-1}}, {'$limit':1});
+grades.aggregate({'$group':{'_id':'$student_id', 'average':{$avg:'$score'}}}, {'$sort':{'average':-1}}, {'$limit':1}, function(err, res) {
+  if (err) {console.log('hi' + err); return mongoose.disconnect();}
+  console.log(res);
+  mongoose.disconnect();
+});
